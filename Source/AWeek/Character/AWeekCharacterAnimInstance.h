4d9282a02fad6224131b3b0d11a93e7fc82eefe8
ChiWon2 <ckw0655@gmail.com>
2025-09-11 07:48:44 +0900

    [MonsterAI] Add Basic Attack, DamageResponse 에셋 변경 ThirdParty->ZombiePack.zip 확인

---
+++ Source/AWeek/Character/AWeekCharacterAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AWeekCharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AWEEK_API UAWeekCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
