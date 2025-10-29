f0c4e35cbb59867b3fce42932a7a48201a5ab148
hyunseo <hansx2079@naver.com>
2025-10-29 22:35:44 +0900

    [SettingSystem] Registry Class 추가

---
+++ Plugins/SettingSystem/Source/SettingSystem/Private/SettingRegistry.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SettingRegistry.h"
+
+void USettingRegistry::Init(ULocalPlayer* InLocalPlayer)
+{
+}
+
+void USettingRegistry::Apply()
+{
+	for (auto Entry:DirtySettings)
+	{
+		USettingItem* Setting = Entry.Value;
+		Setting->Apply();
+	}
+
+	DirtySettings.Reset();
+}
+
+void USettingRegistry::RegisterSetting(USettingItem* Setting)
+{
+	if (Setting)
+	{
+		RootSettings.Add(Setting);
+		InitSetting(Setting);
+	}
+}
+
+void USettingRegistry::InitSetting(USettingItem* Setting)
+{
+	Setting->OnSettingChangedEvent.AddUObject(this, &USettingRegistry::HandleSettingApplied);
+	Setting->OnSettingAppliedEvent.AddUObject(this, &USettingRegistry::HandleSettingChanged);
+	
+	for (USettingItem* ChildSetting: Setting->GetSettings())
+	{
+		InitSetting(ChildSetting);
+	}
+}
+
+void USettingRegistry::HandleSettingChanged(USettingItem* Setting)
+{
+	DirtySettings.Add(FObjectKey(Setting), Setting);
+	OnSettingChanged.Broadcast(Setting);
+}
+
+void USettingRegistry::HandleSettingApplied(USettingItem* Setting)
+{
+}
---
+++ Plugins/SettingSystem/Source/SettingSystem/Public/SettingRegistry.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SettingItem.h"
+#include "UObject/NoExportTypes.h"
+#include "SettingRegistry.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SETTINGSYSTEM_API USettingRegistry : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	DECLARE_EVENT_OneParam(USettingRegistry, FOnSettingChanged, USettingItem*);
+
+	FOnSettingChanged OnSettingChanged;
+
+public:
+	void Init(ULocalPlayer* InLocalPlayer);
+	virtual void Apply();
+
+	void RegisterSetting(USettingItem* Setting);
+	void InitSetting(USettingItem* Setting);
+protected:
+	void HandleSettingChanged(USettingItem* Setting);
+	void HandleSettingApplied(USettingItem* Setting);
+	
+	UPROPERTY()
+	TObjectPtr<ULocalPlayer> OwningLocalPlayer;
+
+	UPROPERTY()
+	TArray<TObjectPtr<USettingItem>> RootSettings;
+
+	TMap<FObjectKey, TObjectPtr<USettingItem>> DirtySettings;
+};
