d7b96f9878b53cf61c35108a5f98f96491f53b40
hyunseo <hansx2079@naver.com>
2025-10-30 13:58:07 +0900

    [AudioSystem] AWeekDeveloper Audio Setting 추가

---
+++ Source/AWeek/Audio/AWeekAudioSettings.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "AWeekAudioSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config=Game, DefaultConfig, meta = (DisplayName = "AWeekAudioSettings"))
+class AWEEK_API UAWeekAudioSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Config, EditAnywhere, Category = "AudioSettings", meta = (AllowedClasses = "SoundClass"))
+	FSoftObjectPath OverallClassPath;
+
+	UPROPERTY(Config, EditAnywhere, Category = "AudioSettings", meta = (AllowedClasses = "SoundClass"))
+	FSoftObjectPath MusicClassPath;
+
+	UPROPERTY(Config, EditAnywhere, Category = "AudioSettings", meta = (AllowedClasses = "SoundClass"))
+	FSoftObjectPath SFXClassPath;
+
+	UPROPERTY(Config, EditAnywhere, Category = "AudioSettings", meta = (AllowedClasses = "SoundClass"))
+	FSoftObjectPath AmbientClassPath;
+
+	UPROPERTY(Config, EditAnywhere, Category = "AudioSettings", meta = (AllowedClasses = "SoundClass"))
+	FSoftObjectPath UIClassPath;
+};
