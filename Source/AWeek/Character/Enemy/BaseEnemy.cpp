9dd90c8ba907f64ef0985dac64f58f832b4cac05
ChiWon2 <ckw0655@gmail.com>
2025-11-20 04:00:13 +0900

    [MonsterAI] EnemyActorSpawner_v1 완성 PoolingSystem 연동 및 생성개수 제한

--- Source/AWeek/Character/Enemy/BaseEnemy.cpp
+++ Source/AWeek/Character/Enemy/BaseEnemy.cpp
@@ -1,130 +1,145 @@
 #include "BaseEnemy.h"
-#include"BaseEnemyAIController.h"
-#include"../AWeekCharacterAnimInstance.h"
-
+#include "BaseEnemyAIController.h"
+#include "../AWeekCharacterAnimInstance.h"
 
 ABaseEnemy::ABaseEnemy()
 {
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
 }
 
 void ABaseEnemy::PossessedBy(AController* NewController)
 {
-	Super::PossessedBy(NewController);
-	m_AIController = Cast<ABaseEnemyAIController>(NewController);
-	if (m_AIController == nullptr)
-		UE_LOG(LogTemp, Log, TEXT("AIC is Empty"));
+    Super::PossessedBy(NewController);
+    m_AIController = Cast<ABaseEnemyAIController>(NewController);
+    if (!m_AIController)
+        UE_LOG(LogTemp, Log, TEXT("AIC is Empty"));
 }
 
 void ABaseEnemy::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	m_AnimInstance = Cast<UAWeekCharacterAnimInstance>(GetMesh()->GetAnimInstance());
-	if (ABaseEnemy* DefaultEnemy = Cast<ABaseEnemy>(GetClass()->GetDefaultObject()))
-	{
-		StatDataAsset = DefaultEnemy->StatDataAsset;
-	}
-	if (!StatDataAsset)
-	{
-		StatDataAsset = Cast<UEnemyStatDataAsset>(
-			StaticLoadObject(UEnemyStatDataAsset::StaticClass(), nullptr,
-				TEXT("/Game/Enemies/Data/DA_EnemyStat.DA_EnemyStat"))
-		);
+    m_AnimInstance = Cast<UAWeekCharacterAnimInstance>(GetMesh()->GetAnimInstance());
 
-		if (!StatDataAsset)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("[%s] Failed to load default StatDataAsset"), *GetName());
-			return;
-		}
-	}
-	m_Stat = StatDataAsset->DayData;
+    if (ABaseEnemy* DefaultEnemy = Cast<ABaseEnemy>(GetClass()->GetDefaultObject()))
+    {
+        StatDataAsset = DefaultEnemy->StatDataAsset;
+    }
 
-	
-	auto& MessageSubsystem = UGameEventMessageSubsystem::Get(this);
+    if (!StatDataAsset)
+    {
+        StatDataAsset = Cast<UEnemyStatDataAsset>(
+            StaticLoadObject(UEnemyStatDataAsset::StaticClass(), nullptr,
+                TEXT("/Game/Enemies/Data/DA_EnemyStat.DA_EnemyStat"))
+        );
 
-	DayChangedListenerHandle = MessageSubsystem.RegisterListener<FDayChangedMessage>(
-		FGameplayTag::RequestGameplayTag(FName("Event.DayChanged")),
-		[this](FGameplayTag Channel, const FDayChangedMessage& Msg)
-		{
-			this->OnDayChanged(Msg);
-		}
-	);
+        if (!StatDataAsset)
+        {
+            UE_LOG(LogTemp, Warning, TEXT("[%s] Failed to load default StatDataAsset"), *GetName());
+            return;
+        }
+    }
+
+    m_Stat = StatDataAsset->DayData;
+
+    auto& MessageSubsystem = UGameEventMessageSubsystem::Get(this);
+
+    DayChangedListenerHandle = MessageSubsystem.RegisterListener<FDayChangedMessage>(
+        FGameplayTag::RequestGameplayTag(FName("Event.DayChanged")),
+        [this](FGameplayTag Channel, const FDayChangedMessage& Msg)
+        {
+            this->OnDayChanged(Msg);
+        }
+    );
 }
 
 void ABaseEnemy::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	if (DayChangedListenerHandle.IsValid())
-	{
-		DayChangedListenerHandle.Unregister();
-	}
+    if (DayChangedListenerHandle.IsValid())
+    {
+        DayChangedListenerHandle.Unregister();
+    }
 
-	Super::EndPlay(EndPlayReason);
+    Super::EndPlay(EndPlayReason);
 }
 
 void ABaseEnemy::OnDayChanged(const FDayChangedMessage& Msg)
 {
-	if (Msg.bIsDay)
-	{
-		UE_LOG(LogTemp, Log, TEXT("[%s] NightEnd!! Day: %d"), *GetName(), Msg.Day);
-		//MovementSpeed Up 
-		m_Stat = StatDataAsset->DayData;
-	}
-	else
-	{
-		UE_LOG(LogTemp, Log, TEXT("[%s] Night Begin!! Day: %d"), *GetName(), Msg.Day);
-		//MovementSpeed Up 
-		m_Stat = StatDataAsset->NightData;
-	}
-	//AI Perception Range Change
-	if (ABaseEnemyAIController* AICon = Cast<ABaseEnemyAIController>(GetController()))
-	{
-		if (!AICon->SightConfig || !AICon->PerceptionComp)
-			return;
-		AICon->ChangeAISightRange(m_Stat.SightRadius, m_Stat.LoseSightRadius,
-			m_Stat.VisionAngleDegrees, m_Stat.MaxAgeSeconds);
+    if (Msg.bIsDay)
+    {
+        UE_LOG(LogTemp, Log, TEXT("[%s] NightEnd!! Day: %d"), *GetName(), Msg.Day);
+        m_Stat = StatDataAsset->DayData;
+    }
+    else
+    {
+        UE_LOG(LogTemp, Log, TEXT("[%s] Night Begin!! Day: %d"), *GetName(), Msg.Day);
+        m_Stat = StatDataAsset->NightData;
+    }
 
-	}
+    // AI Perception Range Change
+    if (ABaseEnemyAIController* AICon = Cast<ABaseEnemyAIController>(GetController()))
+    {
+        if (!AICon->SightConfig || !AICon->PerceptionComp)
+            return;
+
+        AICon->ChangeAISightRange(
+            m_Stat.SightRadius,
+            m_Stat.LoseSightRadius,
+            m_Stat.VisionAngleDegrees,
+            m_Stat.MaxAgeSeconds
+        );
+    }
 }
 
 void ABaseEnemy::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
 
 void ABaseEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
 float ABaseEnemy::GetMoveSpeed_Code(EMovementType type) const
 {
-	return m_Stat.MoveSpeedMap[type];
+    return m_Stat.MoveSpeedMap[type];
 }
 
 float ABaseEnemy::GetMontagePlayRate_Code() const
 {
-	return m_Stat.AnimPlayRate;
+    return m_Stat.AnimPlayRate;
 }
 
 void ABaseEnemy::OnLoadFromPool_Implementation()
 {
-	const FString ClassName = GetClass()->GetName();
+    const FString ClassName = GetClass()->GetName();
 
-	const FString EnemyFolder = FString::Printf(TEXT("Pool/%s"), *ClassName);
-	SetFolderPath(FName(*EnemyFolder));
+    const FString EnemyFolder = FString::Printf(TEXT("Pool/%s"), *ClassName);
+    SetFolderPath(FName(*EnemyFolder));
 
-	// Controller FolderSetting
-	if (Controller)
-	{
-		const FString ControllerFolder = FString::Printf(TEXT("Pool/%s/Controller"), *ClassName);
-		Controller->SetFolderPath(FName(*ControllerFolder));
-	}
+    // Controller FolderSetting
+    if (Controller)
+    {
+        const FString ControllerFolder = FString::Printf(TEXT("Pool/%s/Controller"), *ClassName);
+        Controller->SetFolderPath(FName(*ControllerFolder));
+    }
 }
 
 void ABaseEnemy::OnStoreToPool_Implementation()
 {
+    if (OwnerSpawner)
+    {
+        OnReturnToSpawner.ExecuteIfBound(this);
+    }
 
+    const FString ClassName = GetClass()->GetName();
+    const FString EnemyFolder = FString::Printf(TEXT("Pool/%s"), *ClassName);
+    SetFolderPath(FName(*EnemyFolder));
+
+    if (Controller)
+    {
+        const FString ControllerFolder = FString::Printf(TEXT("Pool/%s/Controller"), *ClassName);
+        Controller->SetFolderPath(FName(*ControllerFolder));
+    }
 }
--- Source/AWeek/Character/Enemy/BaseEnemy.h
+++ Source/AWeek/Character/Enemy/BaseEnemy.h
@@ -2,51 +2,66 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include"../../System/IDamageAble.h"
-#include"../../System/Pool/IPoolable.h"
-#include"../../System/GameEventMessageSubsystem.h"
-#include"../../System/AWeekEventMessageInfo.h"
-#include"EnemyStatDataAsset.h"
+#include "../../System/IDamageAble.h"
+#include "../../System/Pool/IPoolable.h"
+#include "../../System/GameEventMessageSubsystem.h"
+#include "../../System/AWeekEventMessageInfo.h"
+#include "EnemyStatDataAsset.h"
 #include "BaseEnemy.generated.h"
 
+class AEnemyActorSpawner;
 
 UCLASS()
 class AWEEK_API ABaseEnemy : public ACharacter, public IDamageAble, public IPoolable
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	UPROPERTY(BlueprintReadWrite)
-	class ABaseEnemyAIController* m_AIController;
-	UPROPERTY(BlueprintReadWrite)
-	class UAWeekCharacterAnimInstance* m_AnimInstance;
+    UPROPERTY(BlueprintReadWrite)
+    class ABaseEnemyAIController* m_AIController;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category = "BaseEnemy")
-	FEnemyStatData m_Stat;
+    UPROPERTY(BlueprintReadWrite)
+    class UAWeekCharacterAnimInstance* m_AnimInstance;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseEnemy")
-	UEnemyStatDataAsset* StatDataAsset;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseEnemy")
+    FEnemyStatData m_Stat;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseEnemy")
+    UEnemyStatDataAsset* StatDataAsset;
 
 public:
-	ABaseEnemy();
+    ABaseEnemy();
+
 protected:
-	virtual void PossessedBy(AController* NewController) override;
-	virtual void BeginPlay() override;
-	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
-	UFUNCTION()
-	void OnDayChanged(const FDayChangedMessage& Msg);
+    virtual void PossessedBy(AController* NewController) override;
+    virtual void BeginPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+    UFUNCTION()
+    void OnDayChanged(const FDayChangedMessage& Msg);
 
-public:	
-	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	UFUNCTION(BlueprintCallable)
-	float GetMoveSpeed_Code(EMovementType type) const;
-	UFUNCTION(BlueprintCallable)
-	float GetMontagePlayRate_Code() const;
+public:
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    UFUNCTION(BlueprintCallable)
+    float GetMoveSpeed_Code(EMovementType type) const;
+    UFUNCTION(BlueprintCallable)
+    float GetMontagePlayRate_Code() const;
+
 private:
-	FGameEventMessageListenerHandle DayChangedListenerHandle;
-protected:
-	virtual void OnLoadFromPool_Implementation() override;
-	virtual void OnStoreToPool_Implementation() override;
+    FGameEventMessageListenerHandle DayChangedListenerHandle;
 
+    UPROPERTY()
+    AEnemyActorSpawner* OwnerSpawner;
+
+public:
+    //Spawner Delegate
+    DECLARE_DELEGATE_OneParam(FOnReturnToSpawner, ABaseEnemy*);
+    FOnReturnToSpawner OnReturnToSpawner;
+
+    void SetSpawner(AEnemyActorSpawner* Spawner) { OwnerSpawner = Spawner; }
+
+protected:
+    virtual void OnLoadFromPool_Implementation() override;
+    virtual void OnStoreToPool_Implementation() override;
 };
